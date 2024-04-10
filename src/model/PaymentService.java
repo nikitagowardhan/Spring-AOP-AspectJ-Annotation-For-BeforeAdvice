a67c75ff7c8e07cdf7a63580fc8e8454f07091f4
nikitagowardhan <nikitagowar02@gmail.com>
2024-04-10 15:33:49 +0530

    Spring AOP Using Annotation  Before Advice

---
+++ src/model/PaymentService.java
@@ -0,0 +1,16 @@
+package model;
+
+public class PaymentService {
+
+   
+	public void a()
+	{
+		System.out.println("Payment is debited");
+	}
+
+    public int b()
+    {
+    	System.out.println("Payment is credited");
+		return 1000;
+    }
+}
